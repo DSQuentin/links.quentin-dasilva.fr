29906a7e813128b61b341a391d196400f61b8fbf
Quentin <dsquentin@hotmail.fr>
2023-01-27 00:45:18 +0100

    config(tailwind): jit mode, purge

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  purge: {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+  },
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
