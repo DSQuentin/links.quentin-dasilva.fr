6c8ffba4576b871c8d9cd84c2c4304fd942137f9
Quentin <dsquentin@hotmail.fr>
2023-01-27 00:57:55 +0100

    feat: first component, coming soon with return button

---
+++ components/Components.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Components() {
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <p>Coming soon...</p>
+      <a
+        href="https://quentin-dasilva.fr"
+        className="p-2 bg-gray-400 rounded-lg mt-2 hover:bg-gray-100 hover:text-black transition-all "
+      >
+        Return to my website
+      </a>
+    </div>
+  );
+}
+
+export default Components;
---
+++ components/Seo.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Seo() {
+  return (
+    <>
+      <meta
+        name="description"
+        content="Retrouvez tous les liens de mes réseaux."
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+      {/*       <meta property="og:url" content="https://links.corentin-thomas.fr/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Corentin THOMAS | Links" />
+      <meta property="og:description" content="Retrouvez tous les liens vers mes différents réseaux !" />
+      <meta property="og:image" content="https://links.corentin-thomas.fr/card.png" />
+
+      <meta name="twitter:card" content="summary" />
+      <meta property="twitter:domain" content="links.corentin-thomas.fr" />
+      <meta property="twitter:url" content="https://links.corentin-thomas.fr/" />
+      <meta name="twitter:title" content="Corentin THOMAS | Links" />
+      <meta name="twitter:description" content="Retrouvez tous les liens vers mes différents réseaux !" />
+      <meta name="twitter:image" content="https://links.corentin-thomas.fr/card.png" /> */}
+    </>
+  );
+}
+
+export default Seo;
