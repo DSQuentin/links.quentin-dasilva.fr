c5585308861ac009ca834f522b20d692950d248d
Quentin <dsquentin@hotmail.fr>
2023-01-30 18:24:42 +0100

    style(header): add header's style

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
 import React from "react";
 
 function Header() {
   return (
-    <div className="mt-4">
-      <h1>Quentin DA SILVA</h1>
+    <div className="mt-4 text-center flex flex-col items-center">
+      <Image
+        src="https://www.quentin-dasilva.fr/assets/img/testimonials/1.jpg"
+        alt=""
+        height={200}
+        width={200}
+        className="rounded-full h-[6em] w-[6em] border border-white shadow-custom"
+      />
+      <h1 className="inline-block text-[0.8em] font-bold rounded-[1em] bg-[#000a] px-[0.6em] py-[0.3em] mt-2 text-white border border-[#fffa]">
+        Quentin DA SILVA
+      </h1>
     </div>
   );
 }
