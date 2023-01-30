7fb06a605b9976d2d360db4979a547ce9fcebd18
Quentin <dsquentin@hotmail.fr>
2023-01-30 19:17:47 +0100

    feat(background): add Vanta.js background animation

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-sync-scripts */
 import Header from "@/components/Header";
 import GithubIcon from "@/components/icons/GithubIcon";
 import LinkedinIcon from "@/components/icons/LinkedinIcon";
@@ -6,6 +7,7 @@ import WebsiteIcon from "@/components/icons/WebsiteIcon";
 import Link from "@/components/Link";
 import Seo from "@/components/Seo";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function Home() {
   const links = [
@@ -37,14 +39,39 @@ export default function Home() {
 
   return (
     <>
-      <div className="min-h-screen ">
+      <div className="min-h-screen" id="vanta">
+        <Script
+          id="vanta"
+          dangerouslySetInnerHTML={{
+            __html: `
+          VANTA.NET({
+            el: "#vanta",
+            mouseControls: true,
+            touchControls: true,
+            gyroControls: false,
+            minHeight: 300.00,
+            minWidth: 300.00,
+            scale: 1.00,
+            scaleMobile: 1.00,
+            color: 0xf7f7f7,
+            backgroundAlpha: 0.00,
+            points: 8.00,
+            maxDistance: 21.00,
+            spacing: 18.00          
+          })
+                        `,
+          }}
+        />
         <Head>
           <title>Quentin Da Silva | Liens</title>
           <link rel="icon" href="/favicon.ico" />
           <Seo />
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"></script>
+          <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js"></script>
         </Head>
+
         <Header />
-        <ul className="box-border list-none my-0 mx-auto p-[2em]">
+        <ul className="box-border list-none my-0 mx-auto p-[2em] max-w-[480px]">
           {links.map((link) => (
             <Link key={link.name} {...link} />
           ))}
